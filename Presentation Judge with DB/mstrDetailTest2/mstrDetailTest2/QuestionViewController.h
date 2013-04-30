936c79aa841f37fa65549f1307247b0f57fd61a1
salzheimer <scott-alzheimer@uiowa.edu>
2013-04-29 19:28:35 -0500

    image added

---
+++ Presentation Judge with DB/mstrDetailTest2/mstrDetailTest2/QuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.h
+//  mstrDetailTest2
+//
+//  Created by skadoo on 4/29/13.
+//  Copyright (c) 2013 skadoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewController : UIViewController
+
+@end
---
+++ Presentation Judge with DB/mstrDetailTest2/mstrDetailTest2/QuestionViewController.m
@@ -0,0 +1,38 @@
+//
+//  QuestionViewController.m
+//  mstrDetailTest2
+//
+//  Created by skadoo on 4/29/13.
+//  Copyright (c) 2013 skadoo. All rights reserved.
+//
+
+#import "QuestionViewController.h"
+
+@interface QuestionViewController ()
+
+@end
+
+@implementation QuestionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
